ff89ac1b43214d9de08c13e69a06246738874806
Toan Tran <trantoan.fox.97@gmail.com>
2022-09-05 23:33:00 +0700

    feat(system): extract attrs and props from higher-level component

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,5 +1,9 @@
 import { defineComponent, h } from '@vue/runtime-core'
+import { extractStyleAttrs } from './system.attrs'
 import { domElements, DOMElements } from './system.utils'
+import { css as _css, cx } from '@emotion/css'
+import { css } from '@hola-ui/styled-system'
+import theme from '@hola-ui/theme'
 
 /**
  * Creates a Hola UI Vue component
@@ -15,12 +19,18 @@ export const hola: IHolaFactory = (
     inheritAttrs: false,
     props: componentProps,
     setup(props, { slots, attrs }) {
+      const { class: inheritedClass, ...rest } = attrs
+      const { styles, attrs: _attrs } = extractStyleAttrs(rest)
+
+      const className = _css(css(styles)({ theme }))
+
       return () =>
         h(
           tag,
           {
+            class: cx(inheritedClass as string, className),
             ...props,
-            ...attrs,
+            ..._attrs,
           },
           slots
         )
