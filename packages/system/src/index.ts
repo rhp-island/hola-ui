233698937ff637168da5524b569a893959c9ba0e
Toan Tran <trantoan.fox.97@gmail.com>
2022-09-05 23:19:07 +0700

    feat(system): add system architecture

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,26 +1,39 @@
-import {
-  Component,
-  defineComponent,
-  Fragment,
-  Suspense,
-  Teleport,
-  h,
-} from 'vue'
-import { DOMElements } from './system.utils'
+import { defineComponent, h } from '@vue/runtime-core'
+import { domElements, DOMElements } from './system.utils'
 
-type Tag =
-  | string
-  | typeof Fragment
-  | typeof Teleport
-  | typeof Suspense
-  | Component
-
-export const hola = (tag: DOMElements, componentProps = {}) => {
+/**
+ * Creates a Hola UI Vue component
+ * @param tag Tag
+ * @param componentProps Component Props
+ */
+// @ts-ignore
+export const hola: IHolaFactory = (
+  tag: DOMElements,
+  componentProps = {}
+): any => {
   return defineComponent({
     inheritAttrs: false,
     props: componentProps,
     setup(props, { slots, attrs }) {
-      return () => h(tag, {}, slots)
+      return () =>
+        h(
+          tag,
+          {
+            ...props,
+            ...attrs,
+          },
+          slots
+        )
     },
   })
 }
+
+type IHolaFactory = {
+  [key in DOMElements]: any
+} & {
+  (tag: DOMElements): any
+}
+
+domElements.forEach((tag) => {
+  hola[tag] = hola(tag)
+})
