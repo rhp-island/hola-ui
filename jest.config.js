3c620997cb845e5de1e2475dfed00a1750f38218
Toan Tran <trantoan.fox.97@gmail.com>
2022-09-25 10:44:09 +0700

    fix(test): remove hooks testing

--- jest.config.js
+++ jest.config.js
@@ -1,22 +1,20 @@
 module.exports = {
   transform: {
-    "^.+\\.(ts|tsx)$": [
-      "esbuild-jest",
+    '^.+\\.(ts|tsx)$': [
+      'esbuild-jest',
       {
-        jsxFactory: "h",
-        jsxFragment: "Fragment",
+        jsxFactory: 'h',
+        jsxFragment: 'Fragment',
         sourcemap: true,
-        target: "es2020",
+        target: 'es2020',
       },
     ],
   },
-  setupFiles: ["./jest.setup.ts"],
-  transformIgnorePatterns: ["/node_modules/(?!@popperjs/.*|lodash.)"],
+  setupFiles: ['./jest.setup.ts'],
+  transformIgnorePatterns: ['/node_modules/(?!@popperjs/.*|lodash.)'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
-    "@hola-ui/vue-test-utils": "<rootDir>/packages/test-utils",
+    '^@/(.*)$': '<rootDir>/$1',
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  snapshotSerializers: ["@hola-ui/vue-test-utils/src/snapshot-serializer.ts"],
-  testMatch: ["**/**/*.test.(js|jsx|ts|tsx)"],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testMatch: ['**/**/*.test.(js|jsx|ts|tsx)'],
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,41 @@
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
-import Components from "unplugin-vue-components/vite"
-import Pages from "vite-plugin-pages"
-import Layouts from "vite-plugin-vue-layouts"
-import HolaComponents from "./playground/build/components.json"
-import path from "path"
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import Pages from 'vite-plugin-pages'
+import Layouts from 'vite-plugin-vue-layouts'
+import HolaComponents from './playground/build/components.json'
+import path from 'path'
 
 export default defineConfig({
-  test: {
-    globals: true,
-    environment: "happy-dom",
-  },
   esbuild: {
-    jsxFactory: "h",
-    jsxFragment: "Fragment",
+    jsxFactory: 'h',
+    jsxFragment: 'Fragment',
   },
   optimizeDeps: {
-    exclude: ["@popperjs/core", "@vueuse/core", "@vueuse/motion"],
+    exclude: ['@popperjs/core', '@vueuse/core', '@vueuse/motion'],
   },
   build: {
-    target: "modules",
+    target: 'modules',
   },
   server: {
     watch: {
-      ignored: ["**/*snapshots*"],
+      ignored: ['**/*snapshots*'],
     },
   },
   resolve: {
-    alias: {
-      "@hola-ui/vue-test-utils": path.resolve(
-        __dirname,
-        "./packages/test-utils"
-      ),
-    },
+    alias: {},
   },
   plugins: [
     vue(),
     Pages({
-      pagesDir: path.relative(__dirname, "../packages"),
-      extensions: ["vue"],
+      pagesDir: path.relative(__dirname, '../packages'),
+      extensions: ['vue'],
       extendRoute(route, parent) {
-        if (route.path === "/") {
+        if (route.path === '/') {
           return route
         }
-        const [groupRaw] = route.name!.split("-examples-")
-        const [_, group] = groupRaw.split("c-")
+        const [groupRaw] = route.name!.split('-examples-')
+        const [_, group] = groupRaw.split('c-')
         return {
           ...route,
           groupRaw,
@@ -54,7 +45,7 @@ export default defineConfig({
       },
     }),
     Layouts({
-      layoutsDir: "/layouts",
+      layoutsDir: '/layouts',
     }),
     Components({
       resolvers: [
